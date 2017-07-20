e91d2721145efa49c3430aba2fff6c32620c141c
Perry Gowdy <pgowdy1@gmail.com>
2017-07-19 22:26:37 -0500

    Alpha is up and running -- functionality is implemented for a few level 1 spells.

--- src/index.js
+++ src/index.js
@@ -1,4 +1,4 @@
-var spells = require("./spells");
+var junkForNow = require("./spells"); //Need to fix this and seperate Spells object.
 var Alexa = require('alexa-sdk');
 
 exports.handler = function(event, context, callback){
@@ -9,7 +9,8 @@ exports.handler = function(event, context, callback){
 
 var applicationString = {
     "OPEN_SPELL_BOOK": "Welcome to the Pathfinder Spellbook." +
-        "Please ask a question such as \'look up mage hand\' or \'find dancing lights\'."
+        "Please ask a question such as \'look up mage hand\' or \'find dancing lights\'.",
+    "CANCEL_MESSAGE": "I will return the spell book to the library master.",
 }
 
 var SPELL_BOOK_STATES = {
@@ -21,29 +22,80 @@ var newSessionHandlers = {
     "LaunchRequest": function () {
         this.handler.state = SPELL_BOOK_STATES.OPEN;
         this.emitWithState("OpenSpellBook", true);
+    },
+    "AMAZON.StartOverIntent": function() {
+        //TODO
+    },
+    "AMAZON.HelpIntent": function() {
+        //TODO
+    },
+    "Unhandled": function () {
+        //TODO
     }
 };
 
 var startStateHandlers = Alexa.CreateStateHandler(SPELL_BOOK_STATES.OPEN, {
     "OpenSpellBook": function (newSpell) {
-        var speechOutput = "Welcome to the Pathfinder Spellbook." +
-            "Please ask a question such as \'look up mage hand\' or \'find dancing lights\'.";
+        var speechOutput = "Welcome to the Pathfinder Spellbook. <break time='1s'/> Please ask a question such as \'look up mage hand\' or \'find dancing lights\'.";
         this.handler.state = SPELL_BOOK_STATES.SEARCH;
-        this.emit(':tell', speechOutput);
+        this.emit(':ask', speechOutput);
     }
 });
+
+var spells =
+    {
+        daze: "humanoid creature of 4 hd or less loses next action.",
+        flare: "dazzles one creature (–1 on attack rolls).",
+        light: "object shines like a torch.",
+        lullaby: "makes subject drowsy: –5 on perception checks, –2 on will saves against sleep.",
+        magehand: "5-pound telekinesis.",
+        mending: "makes minor repairs on an object.",
+        message: "whisper conversation at distance.",
+    };
+
 
 var spellBookHandlers = Alexa.CreateStateHandler(SPELL_BOOK_STATES.SEARCH, {
     "OneshotSpellIntent": function () {
         handleUserAsk.call(this);
+    },
+    "DontKnowIntent": function () {
+        //TODO
+    },
+    "AMAZON.StartOverIntent": function () {
+        //TODO
+    },
+    "AMAZON.RepeatIntent": function () {
+        //TODO
+    },
+    "AMAZON.HelpIntent": function () {
+        //TODO
+    },
+    "AMAZON.StopIntent": function () {
+        //TODO
+    },
+    "AMAZON.CancelIntent": function () {
+        this.emit(":tell", this.t("CANCEL_MESSAGE"));
+    },
+    "Unhandled": function () {
+        //TODO
+    },
+    "SessionEndedRequest": function () {
+        console.log("Session ended in trivia state: " + this.event.request.reason);
     }
 });
 
 function handleUserAsk() {
-    var spellSlotIsValid = isSpellSlotValid(this.event.request.intent);
-    var speechOutput = "";
-    var speechOutputAnalysis = "";
-
+    var intent = this.event.request.intent;
+    var spellSlotIsValid = isSpellSlotValid(intent);
+    if(spellSlotIsValid)
+    {
+        var spellToLookUp = intent.slots.Spell.value;
+        var speechOutput = spells[spellToLookUp];
+        this.emit(':ask', speechOutput);
+    }
+    else {
+        this.emit(':tell', "Sorry, I couldn't find that spell.");
+    }
 }
 
 function isSpellSlotValid(intent) {
---
+++ src/spells.js
@@ -0,0 +1,18 @@
+"use strict";
+
+module.exports = {
+    "SPELLS" :
+        {
+            "dancing lights": "creates torches or other lights.",
+            "daze": "humanoid creature of 4 hd or less loses next action.",
+            "detect magic": "detects spells and magic items within 60 ft.",
+            "flare": "dazzles one creature (–1 on attack rolls).",
+            "ghost sound": "figment sounds.",
+            "know direction": "you discern north.",
+            "light": "object shines like a torch.",
+            "lullaby": "makes subject drowsy: –5 on perception checks, –2 on will saves against sleep.",
+            "mage hand": "5-pound telekinesis.",
+            "mending": "makes minor repairs on an object.",
+            "message": "whisper conversation at distance.",
+        }
+};
